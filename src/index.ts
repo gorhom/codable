ef93a3caffbd7318aeb81a86e2fca4aa79e5a204
Gorhom <gorhom@me.com>
2019-08-06 17:57:22 +0200

    chore: add initial codable class

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,60 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
+
+type CodableType = 'string' | 'number' | 'array' | typeof Codable;
+
+export class Codable {
+  static CodingKeys: { [key: string]: CodableType };
+
+  // @ts-ignore
+  constructor(payload: object) {
+    // @ts-ignore
+    const keys = Object.keys(this.__proto__.constructor.CodingKeys);
+
+    keys.forEach(key => {
+
+      // @ts-ignore
+      const type = this.__proto__.constructor.CodingKeys[key]
+
+      // @ts-ignore
+      if (payload[key] === undefined) {
+        throw new Error(`missing '${key}'`);
+      }
+
+      if(type.prototype instanceof Codable) {
+        // @ts-ignore
+        const object = Object.create(type.prototype)
+        // @ts-ignore
+        this[key] = object.constructor.call(object, payload[key])
+        return
+      }
+      // @ts-ignore
+      this[key] = payload[key];
+    });
+  }
+}
+
+export class User extends Codable {
+  username!: string;
+  name!: FullName;
+}
+
+class FullName extends Codable {
+  firstName!: string;
+  lastName!: string;
+
+  get fullname() {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+FullName.CodingKeys = {
+  firstName: 'string',
+  lastName: 'string'
+}
+
+User.CodingKeys = {
+  username: 'string',
+  name: FullName
+};
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,17 @@
-import { sum } from '../src';
+import { sum, User } from '../src';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
   });
+
+  const user = new User({
+    username: 'Gorhom',
+    name: {
+      firstName: 'Mo',
+      lastName: 'Gorhom',
+    },
+  });
+
+  console.debug(user.name.fullname);
 });
